35a329c0d9858cea62ef7f3c5fe32e62f05cad37
Santiconquest <161180110+Santiconquest@users.noreply.github.com>
2024-06-05 02:45:01 +0000

    Se genera lista de tareas pendientes en react

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [task, setTask] = useState("");
+	const [todos, setTodos] = useState([]);
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center p-5">
+			<h1 className="text-center mt-5">Todo list!</h1>
+			<ul>
+				<li>
+					<input 
+						type="text" 
+						placeholder="Ingrese tarea a realizar" 
+						onChange={(event)=> {setTask(event.target.value)}}
+						value={task}
+						onKeyDown={(event)=>{if (event.key == "Enter"){
+							 setTodos(todos.concat(task));
+							setTask("");		
+					}}}
+					/>
+				</li>
+				{todos.map((item,index) => (
+					<li className="d-flex justify-content-between">
+						 {item} {" "}
+						 <i 
+						 class="fas fa-times" 
+						onClick={()=> setTodos(todos.filter(
+								(t,currentIndex) => index!=currentIndex
+				))}>
+						 </i>
+					</li>
+				))}
+			</ul>
+			<span>
+				{todos.length} tareas pendientes
+			</span>
 		</div>
+		
 	);
 };
 
